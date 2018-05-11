95ad42e02501573e6fe3d29ead44099d8ec7c1d3
tahiru <developer.usman.tahir@gmail.com>
2018-05-11 06:51:44 -0400

    feat: refactored the DSSet class to use ES6 class syntax

--- set/DSSet.js
+++ set/DSSet.js
@@ -1,71 +1,71 @@
 
-function DSSet() {
-    this.values = [];
-    this.numberOfValues = 0;
-}
-
-DSSet.prototype.add = function(value) {
-    if (!this.contains(value)) {
-        this.values.push(value);
-        this.numberOfValues++;
+class DSSet {
+    constructor() {
+        this.values = [];
+        this.numberOfValues = 0;
     }
-}
 
-DSSet.prototype.remove = function(value) {
-    if (this.contains(value)) {
-        let index = this.values.indexOf(value);
-
-        this.values.splice(index, 1);
-        this.numberOfValues--;
+    contains(data) {
+        return this.values.indexOf(data) !== -1;
     }
-}
 
-DSSet.prototype.contains = function(value) {
-    return this.values.indexOf(value) !== -1;
-}
-
-DSSet.prototype.union = function(set) {
-    let newSet = new DSSet();
-    let combinedSets = this.values.concat(set.values);
-
-    combinedSets.forEach(function(value) {
-        newSet.add(value);
-    });
-    return newSet;
-}
-
-DSSet.prototype.intersect = function(set) {
-    let newSet = new DSSet;
-    this.values.forEach(function(value) {
-        if (set.contains(value)) {
-            newSet.add(value);
+    add(data) {
+        if (!this.contains(data)) {
+            this.values.push(data);
+            this.numberOfValues++;
         }
-    });
-    return newSet;
-}
+    }
 
-DSSet.prototype.difference = function(set) {
-    let newSet = new DSSet();
-    this.values.forEach(function(value) {
-        if (!set.contains(value)) {
-            newSet.add(value);
+    remove(data) {
+        if (this.contains(data)) {
+            let index = this.values.indexOf(data);
+            this.values.splice(index, 1);
+            this.numberOfValues--;
         }
-    });
-    return newSet;
-}
+    }
 
-DSSet.prototype.isSubset = function(set) {
-    return set.values.every(function(value) {
-        return this.contains(value);
-    }, this);
-}
+    union(set) {
+        let newSet = new DSSet();
+        let combinedSet = this.values.concat(set.values);
+        combinedSet.forEach((value) => {
+            newSet.add(value);
+        });
+        return newSet;
+    }
 
-DSSet.prototype.length = function() {
-    return this.numberOfValues;
-}
+    intersect(set) {
+        let newSet = new DSSet();
+        this.values.forEach((value) => {
+            if (set.contains(value)) {
+                newSet.add(value);
+            }
+        });
+        return newSet;
+    }
 
-DSSet.prototype.print = function() {
-    return this.values.join(' ');
+    difference(set) {
+        let newSet = new DSSet();
+        this.values.forEach((value) => {
+            if (!set.contains(value)) {
+                newSet.add(value);
+            }
+        });
+        return newSet;
+    }
+
+    isSubset(set) {
+        return set.values.every((value) => {
+            return this.contains(value);
+        }, this);
+    }
+
+    length() {
+        return this.numberOfValues;
+    }
+
+    print() {
+        return this.values.join(' ');
+    }
 }
 
 module.exports = DSSet;
