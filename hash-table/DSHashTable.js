88fd0f7539c2ad086d7db58320652d3f730d40e4
tahiru <developer.usman.tahir@gmail.com>
2018-05-11 06:37:59 -0400

    feat: refactored the DSArray class to use ES6 class syntax

--- hash-table/DSHashTable.js
+++ hash-table/DSHashTable.js
@@ -1,57 +1,58 @@
 
-function DSHashTable(size) {
-    this.values = {};
-    this.numberOfValues = 0;
-    this.size = size || 0;
-}
-
-DSHashTable.prototype.calculateHash = function(key) {
-    return key.toString().length % this.size;
-}
-
-DSHashTable.prototype.add = function(key, value) {
-    let hash = this.calculateHash(key);
-
-    if (!this.values.hasOwnProperty(hash)) {
-        this.values[hash] = {};
+class DSHashTable {
+    constructor(size = 0) {
+        this.values = {};
+        this.numberOfValues = 0;
+        this.size = size;
     }
 
-    if (!this.values[hash].hasOwnProperty(key)) {
-        this.numberOfValues++;
+    calculateHash(key) {
+        return key.toString().length % this.size;
     }
 
-    this.values[hash][key] = value;
-}
+    add(key, value) {
+        let hash = this.calculateHash(key);
+        if (!this.values.hasOwnProperty(hash)) {
+            this.values[hash] = {};
+        }
 
-DSHashTable.prototype.remove = function(key) {
-    let hash = this.calculateHash(key);
+        if(!this.values[hash].hasOwnProperty(key)) {
+            this.numberOfValues++;
+        }
 
-    if (this.values.hasOwnProperty(hash) && this.values[hash].hasOwnProperty(key)) {
-        delete this.values[hash][key];
-        this.numberOfValues--;
+        this.values[hash][key] = value;
     }
-}
 
-DSHashTable.prototype.search = function(key) {
-    let hash = this.calculateHash(key);
-    if (this.values.hasOwnProperty(hash) && this.values[hash].hasOwnProperty(key)) {
-        return this.values[hash][key];
-    }
-    return null;
-}
+    remove(key) {
+        let hash = this.calculateHash(key);
 
-DSHashTable.prototype.length = function() {
-    return this.numberOfValues;
-}
-
-DSHashTable.prototype.print = function() {
-    let string = '';
-    for (let value in this.values) {
-        for (let key in this.values[value]) {
-            string += this.values[value][key] + ' ';
+        if(this.values.hasOwnProperty(hash) && this.values[hash].hasOwnProperty(key)) {
+            delete this.values[hash][key];
+            this.numberOfValues--;
         }
     }
-    return string.trim();
+
+    search(key) {
+        let hash = this.calculateHash(key);
+        if (this.values.hasOwnProperty(hash) && this.values[hash].hasOwnProperty(key)) {
+            return this.values[hash][key];
+        }
+        return null;
+    }
+
+    length() {
+        return this.numberOfValues;
+    }
+
+    print() {
+        let string = '';
+        for (let value in this.values) {
+            for (let key in this.values[value]) {
+                string += `${this.values[value][key]} `;
+            }
+        }
+        return string.trim();
+    }
 }
 
 module.exports = DSHashTable;
