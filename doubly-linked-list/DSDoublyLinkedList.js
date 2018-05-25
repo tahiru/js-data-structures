16d66b8da9e663549eabf1d01b2cf0de44579990
tahiru <developer.usman.tahir@gmail.com>
2018-05-25 03:59:03 -0400

    feat: refactored the DSDoublyLinkedList class to use ES6 class syntax

--- doubly-linked-list/DSDoublyLinkedList.js
+++ doubly-linked-list/DSDoublyLinkedList.js
@@ -1,121 +1,125 @@
 
-function Node(data) {
-    this.data = data;
-    this.previous = null;
-    this.next = null;
-}
-
-function DSDoublyLinkedList() {
-    this.head = null;
-    this.tail = null;
-    this.numberOfValues = 0;
-}
-
-DSDoublyLinkedList.prototype.add = function(data) {
-    let node = new Node(data);
-
-    if (!this.head) {
-        this.head = node;
-        this.tail = node;
-    } else {
-        node.previous = this.tail;
-        this.tail.next = node;
-        this.tail = node;
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.previous = null;
+        this.next = null;
     }
-    this.numberOfValues += 1;
 }
 
-DSDoublyLinkedList.prototype.remove = function(data) {
-    let current = this.head;
+class DSDoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.numberOfValues = 0;
+    }
 
-    while (current) {
-        if (current.data === data) {
-            if (current === this.head && current === this.tail) {
-                this.head = null;
-                this.tail = null;
-            } else if (current === this.head) {
-                this.head = this.head.next;
-                this.head.previous = null;
-            } else if (current === this.tail) {
-                this.tail = this.tail.previous;
-                this.tail.next = null;
-            } else {
-                current.previous.next = current.next;
-                current.next.previous = current.previous;
+    add(data) {
+        let node = new Node(data);
+
+        if (!this.head) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            node.previous = this.tail;
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.numberOfValues += 1;
+    }
+
+    remove(data) {
+        let current = this.head;
+        
+        while (current) {
+            if (current.data === data) {
+                if (current === this.head && current === this.tail) {
+                    this.head = null;
+                    this.tail = null;
+                } else if (current === this.head) {
+                    this.head = this.head.next;
+                    this.head.previous = null;
+                } else if (current === this.tail) {
+                    this.tail = this.tail.previous;
+                    this.tail.next = null;
+                } else {
+                    current.previous.next = current.next;
+                    current.next.previous = current.previous;
+                }
+                this.numberOfValues -= 1;
             }
-            this.numberOfValues -= 1;
+            current = current.next;
         }
-        current = current.next;
     }
-}
 
-DSDoublyLinkedList.prototype.insert = function(data, toNodeData) {
-    let current = this.head;
+    insert(data, toNodeData) {
+        let current = this.head;
 
-    while (current) {
-        if (current.data === toNodeData) {
-            let node = new Node(data);
+        while (current) {
+            if (current.data === toNodeData) {
+                let node = new Node(data);
 
-            if (current === this.tail) {
-                this.add(data);
-            } else {
-                current.next.previous = node;
-                node.previous = current;
-                node.next = current.next;
-                current.next = node;
-                this.numberOfValues += 1;
+                if (current === this.tail) {
+                    this.add(data);
+                } else {
+                    current.next.previous = node;
+                    node.previous = current;
+                    node.next = current.next;
+                    current.next = node;
+                    this.numberOfValues += 1;
+                }
             }
+            current = current.next;
         }
-        current = current.next;
     }
-}
 
-DSDoublyLinkedList.prototype.traverse = function(callback) {
-    let current = this.head;
+    traverse(callback) {
+        let current = this.head;
 
-    while (current) {
-        if (callback) {
-            callback(current);
+        while (current) {
+            if (callback) {
+                callback(current);
+            }
+            current = current.next;
         }
-        current = current.next;
     }
-}
 
-DSDoublyLinkedList.prototype.traverseReverse = function(callback) {
-    let current = this.tail;
+    traverseReverse(callback) {
+        let current = this.tail;
 
-    while (current) {
-        if (callback) {
-            callback(current);
+        while (current) {
+            if (callback) {
+                callback(current);
+            }
+            current = current.previous;
         }
-        current = current.previous;
     }
-}
 
-DSDoublyLinkedList.prototype.length = function() {
-    return this.numberOfValues;
-}
-
-DSDoublyLinkedList.prototype.print = function() {
-    let string = '';
-    let current = this.head;
-
-    while (current) {
-        string += `${current.data} `;
-        current = current.next;
+    length() {
+        return this.numberOfValues;
     }
-    return string.trim();
-}
 
-DSDoublyLinkedList.prototype.printReverse = function() {
-    let string = '';
-    let current = this.tail;
+    print() {
+        let string = '';
+        let current = this.head;
 
-    while (current) {
-        string += `${current.data} `;
-        current = current.previous;
+        while (current) {
+            string += `${current.data} `;
+            current = current.next;
+        }
+        return string.trim();
     }
-    return string.trim();
+
+    printReverse() {
+        let string = '';
+        let current = this.tail;
+
+        while (current) {
+            string += `${current.data} `;
+            current = current.previous;
+        }
+        return string.trim();
+    }
 }
 
 module.exports = DSDoublyLinkedList;
